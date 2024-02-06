4b2523518e7cc16808e3382b3fc4eff1027b28e8
Oleh <oleh@broadlanddigital.co.uk>
2024-02-06 15:09:27 +0200

    added funcs to work with json

--- lesson-2/work-with-json-files/app.js
+++ lesson-2/work-with-json-files/app.js
@@ -1,24 +1,55 @@
-const books = require("./books");
+//MY CODE
+const books = require("./books")
 
-const invokeAction = async ({action, id, title, author}) => {
-    switch(action) {
+const invokeAction = async ({ action, id, title, author }) => {
+    switch (action) {
         case "read":
-            const allBooks = await books.getAll();
-            return console.log(allBooks);
+            const allBooks = await books.getAll()
+            return console.log("allBooks==>", allBooks)
+
         case "getById":
-            const oneBook = await books.getById(id);
-            return console.log(oneBook);
+            const book = await books.getById(id)
+            return console.log("book==>", book)
         case "add":
-            const newBook = await books.add({title, author});
-            return console.log(newBook);
+            const newBook = await books.add({ title, author })
+            return console.log("newBook==>", newBook)
         case "updateById":
-            const updateBook = await books.updateById(id, {title, author});
-            return console.log(updateBook);
+            const updateBook = await books.updateById(id, { title, author })
+            return console.log("updateBook==>", updateBook)
         case "deleteById":
-            const deleteBook = await books.deleteById(id);
-            return console.log(deleteBook);
+            const deleteBook = await books.deleteById(id)
+            return console.log("deletedBook==>", deleteBook)
     }
 }
+
+// invokeAction({ action: "add", title: "First Law", author: "Joe Abercrombie" })
+// invokeAction({ action: "read" })
+// invokeAction({ action: "getById", id: "u9kgwNWGi3uUUwh0b8V49" })
+// invokeAction({ action: "updateById", id: "DGRH1KZzLQZOyT3wMUOah", title: "Heroes", author: "Joe A." })
+// invokeAction({ action: "deleteById", id: "UJQTw3SYFikv3MY0EZtaB", })
+
+//Was before
+// const books = require("./books");
+
+// const invokeAction = async ({action, id, title, author}) => {
+//     switch(action) {
+//         case "read":
+//             const allBooks = await books.getAll();
+//             return console.log(allBooks);
+//         case "getById":
+//             const oneBook = await books.getById(id);
+//             return console.log(oneBook);
+//         case "add":
+//             const newBook = await books.add({title, author});
+//             return console.log(newBook);
+//         case "updateById":
+//             const updateBook = await books.updateById(id, {title, author});
+//             return console.log(updateBook);
+//         case "deleteById":
+//             const deleteBook = await books.deleteById(id);
+//             return console.log(deleteBook);
+//     }
+// }
 
 // invokeAction({action: "read"});
 // invokeAction({action: "getById", id: "u9kgwNWGi3uUUwh0b8V49"});
--- lesson-2/work-with-json-files/books/index.js
+++ lesson-2/work-with-json-files/books/index.js
@@ -1,51 +1,62 @@
-const fs = require("fs/promises");
-const path = require("path");
-const {nanoid} = require("nanoid");
+//MY CODE
+const fs = require("fs/promises")
+const path = require("path")
+const { nanoid } = require("nanoid")
 
-const booksPath = path.join(__dirname, "books.json");
+const booksPath = path.join(__dirname, "books.json")
 
 const getAll = async () => {
-    const data = await fs.readFile(booksPath);
-    return JSON.parse(data);
+    const data = await fs.readFile(booksPath)
+    return JSON.parse(data)
 }
 
 const getById = async (id) => {
-    const books = await getAll();
-    const result = books.find(item => item.id === id);
-    return result || null;
+    const books = await getAll()
+    const book = books.find(book => book.id === id)
+    return book || null
 }
 
-const add = async(data) => {
-    const books = await getAll();
+const add = async (data) => {
+    const books = await getAll()
     const newBook = {
         id: nanoid(),
-        ...data,
+        ...data
     }
-    books.push(newBook);
-    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-    return newBook;
+    books.push(newBook)
+    await fs.writeFile(booksPath, JSON.stringify(books, null, 2)) // argumens for that json file not be in one string
+    return newBook
 }
 
-const updateById = async(id, data) => {
-    const books = await getAll();
-    const index = books.findIndex(item => item.id === id);
-    if(index === -1){
-        return null;
+const updateById = async (id, data) => {
+    const books = await getAll()
+    const index = books.findIndex(item => item.id === id)
+    if (index === -1) {
+        return null
     }
-    books[index] = {id, ...data};
-    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-    return books[index];
+    books[index] = { id, ...data }
+    await fs.writeFile(booksPath, JSON.stringify(books, null, 2))
+    return books[index]
+
+    //my variant, but not sure that correct
+    // const updatedBook = {
+    //     id,
+    //     ...data
+    // }
+    // const newAllBooks = await books.filter(item => item.id !== id)
+    // newAllBooks.push(updatedBook)
+    // await fs.writeFile(booksPath, JSON.stringify(newAllBooks, null, 2))
+    // return updatedBook
 }
 
-const deleteById = async(id) => {
-    const books = await getAll();
-    const index = books.findIndex(item => item.id === id);
-    if(index === -1){
-        return null;
+const deleteById = async (id) => {
+    const books = await getAll()
+    const index = books.findIndex(item => item.id === id)
+    if (index === -1) {
+        return null
     }
-    const [result] = books.splice(index, 1);
-    await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
-    return result;
+    const [result] = books.splice(index, 1)
+    fs.writeFile(booksPath, JSON.stringify(books, null, 2))
+    return result
 }
 
 module.exports = {
@@ -53,5 +64,65 @@ module.exports = {
     getById,
     add,
     updateById,
-    deleteById,
+    deleteById
 }
+
+
+//WAS BEFORE
+// const fs = require("fs/promises");
+// const path = require("path");
+// const {nanoid} = require("nanoid");
+
+// const booksPath = path.join(__dirname, "books.json");
+
+// const getAll = async () => {
+//     const data = await fs.readFile(booksPath);
+//     return JSON.parse(data);
+// }
+
+// const getById = async (id) => {
+//     const books = await getAll();
+//     const result = books.find(item => item.id === id);
+//     return result || null;
+// }
+
+// const add = async(data) => {
+//     const books = await getAll();
+//     const newBook = {
+//         id: nanoid(),
+//         ...data,
+//     }
+//     books.push(newBook);
+//     await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+//     return newBook;
+// }
+
+// const updateById = async(id, data) => {
+//     const books = await getAll();
+//     const index = books.findIndex(item => item.id === id);
+//     if(index === -1){
+//         return null;
+//     }
+//     books[index] = {id, ...data};
+//     await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+//     return books[index];
+// }
+
+// const deleteById = async(id) => {
+//     const books = await getAll();
+//     const index = books.findIndex(item => item.id === id);
+//     if(index === -1){
+//         return null;
+//     }
+//     const [result] = books.splice(index, 1);
+//     await fs.writeFile(booksPath, JSON.stringify(books, null, 2));
+//     return result;
+// }
+
+// module.exports = {
+//     getAll,
+//     getById,
+//     add,
+//     updateById,
+//     deleteById,
+// }
